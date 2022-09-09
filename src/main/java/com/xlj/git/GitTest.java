78befa18ac5054e6aaa0e25815d4e0be948b3280
xlj <1511224024@qq.com>
2022-09-09 14:11:24 +0800

    first commit

---
+++ src/main/java/com/xlj/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xlj.git;
+
+/**
+ * @author cxk12138
+ * @create 2022-09-09-14:07
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
