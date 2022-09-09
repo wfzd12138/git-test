a7c2d784c879ad08ba7fc2639cba7d886696f170
xlj <1511224024@qq.com>
2022-09-09 14:36:25 +0800

    hot-fix commit1

--- src/main/java/com/xlj/git/GitTest.java
+++ src/main/java/com/xlj/git/GitTest.java
@@ -1,6 +1,9 @@
 package com.xlj.git;
 
 /**
+ *  绿色标签的个数表示当前有几个分支，且绿色标签都是指向最新版本
+ *  黄色表示当前查看的是哪个版本的代码
+ *
  * @author cxk12138
  * @create 2022-09-09-14:07
  */
@@ -10,5 +13,6 @@ public class GitTest {
         System.out.println("hello,git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("hello git4!");
     }
 }
